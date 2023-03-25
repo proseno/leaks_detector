2d2b57d6f3d1ff1845c3d6d0f78e548785be967d
proseno <savorskij201@gmail.com>
2023-03-25 19:07:14 +0200

    move files into code dir

--- code/simple.py
+++ code/simple.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-
-# import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
@@ -11,6 +10,6 @@ model.compile(optimizer='sgd', loss='mean_squared_error')
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
-model.fit(xs, ys, epochs=45)
+model.fit(xs, ys, epochs=500)
 
 print(model.predict([10.0]))
